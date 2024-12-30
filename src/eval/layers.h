c6dd355db2a1c325f956028bcc198c6b5a2fe3e9
Anton Kernazhytski <kernozh_ant2003@mail.ru>
2024-12-30 21:29:47 +0300

    Embedded model into code

--- src/eval/layers.h
+++ src/eval/layers.h
@@ -1,22 +1,57 @@
 #ifndef QUIRKY_SRC_EVAL_LAYERS_H
 #define QUIRKY_SRC_EVAL_LAYERS_H
 
-// Code is inherited from: https://github.com/official-stockfish/nnue-pytorch/blob/master/docs/nnue.md
+// Code is inherited from:
+// https://github.com/official-stockfish/nnue-pytorch/blob/master/docs/nnue.md
 
 #include <immintrin.h>
 
 #include <array>
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
 
 #include "../util/macro.h"
+#include "embed/model_weights.h"
 
 namespace q_eval {
 
+static constexpr float WEIGHT_SCALE = 64;
+static constexpr float ACTIVATION_SCALE = 127;
+static constexpr float OUTPUT_SCALE = 64;
+
+struct ModelReader {
+  public:
+    template <std::integral T>
+    T ReadWeight(int scale) {
+        float weight = MODEL_WEIGHTS[index_++];
+        int64_t final_weight = std::round(weight * scale);
+        final_weight = std::min(final_weight, static_cast<int64_t>(std::numeric_limits<T>::max()));
+        final_weight = std::max(final_weight, static_cast<int64_t>(std::numeric_limits<T>::min()));
+        return final_weight;
+    }
+
+  private:
+    size_t index_ = 0;
+};
+
 template <size_t INPUT_SIZE, size_t OUTPUT_SIZE>
 struct FeatureLayer {
-    alignas(32) std::array<std::array<int16_t, OUTPUT_SIZE>, INPUT_SIZE> weights;
-    alignas(32) std::array<int16_t, OUTPUT_SIZE> biases;
+  public:
+    void Initialize(ModelReader& reader) {
+        for (size_t i = 0; i < INPUT_SIZE; i++) {
+            for (size_t j = 0; j < OUTPUT_SIZE; j++) {
+                weights_[i][j] = reader.ReadWeight<int16_t>(ACTIVATION_SCALE);
+            }
+        }
+        for (size_t i = 0; i < OUTPUT_SIZE; i++) {
+            biases_[i] = reader.ReadWeight<int16_t>(ACTIVATION_SCALE);
+        }
+    }
+
+    void GetResultOnEmptyBoard(std::array<int16_t, OUTPUT_SIZE>& output) {
+        std::copy(biases_.begin(), biases_.end(), output.begin());
+    }
 
     void Update(int16_t* input, size_t position, int8_t delta) {
         static constexpr int REGISTER_WIDTH = 256 / 16;
@@ -28,25 +63,76 @@ struct FeatureLayer {
         if (delta == -1) {
             for (uint16_t i = 0; i < NUMBER_OF_CHUNKS; i++) {
                 regs[i] = _mm256_sub_epi16(
-                    regs[i], _mm256_load_si256((__m256i*)(&weights[position][i * REGISTER_WIDTH])));
+                    regs[i],
+                    _mm256_load_si256((__m256i*)(&weights_[position][i * REGISTER_WIDTH])));
             }
         } else {
             for (uint16_t i = 0; i < NUMBER_OF_CHUNKS; i++) {
                 regs[i] = _mm256_add_epi16(
-                    regs[i], _mm256_load_si256((__m256i*)(&weights[position][i * REGISTER_WIDTH])));
+                    regs[i],
+                    _mm256_load_si256((__m256i*)(&weights_[position][i * REGISTER_WIDTH])));
             }
         }
         for (uint16_t i = 0; i < NUMBER_OF_CHUNKS; i++) {
             _mm256_store_si256((__m256i*)(&input[i * REGISTER_WIDTH]), regs[i]);
         }
     }
+
+  private:
+    alignas(32) std::array<std::array<int16_t, OUTPUT_SIZE>, INPUT_SIZE> weights_;
+    alignas(32) std::array<int16_t, OUTPUT_SIZE> biases_;
 };
 
 template <size_t INPUT_SIZE, size_t OUTPUT_SIZE>
 struct LinearLayer {
   public:
-    alignas(32) std::array<int8_t, INPUT_SIZE * OUTPUT_SIZE> weights;
-    alignas(32) std::array<int32_t, OUTPUT_SIZE> biases;
+    void Initialize(ModelReader& reader) {
+        for (size_t i = 0; i < INPUT_SIZE; i++) {
+            for (size_t j = 0; j < OUTPUT_SIZE; j++) {
+                if (i >= 16) {
+                    weights_[j * INPUT_SIZE + i] = 0;
+                    continue;
+                }
+                weights_[j * INPUT_SIZE + i] = reader.ReadWeight<int8_t>(WEIGHT_SCALE);
+            }
+        }
+        for (size_t i = 0; i < OUTPUT_SIZE; i++) { 
+            biases_[i] = reader.ReadWeight<int32_t>(ACTIVATION_SCALE * WEIGHT_SCALE);
+        }
+    }
+
+    void Process(const int8_t* input, int32_t* output) {
+        static constexpr int REGISTER_WIDTH = 256 / 8;
+        constexpr int NUMBER_OF_INPUT_CHUNKS = INPUT_SIZE / REGISTER_WIDTH;
+        constexpr int NUMBER_OF_OUTPUT_CHUNKS = OUTPUT_SIZE / 4;
+
+        for (int i = 0; i < NUMBER_OF_OUTPUT_CHUNKS; i++) {
+            const int offset0 = (i * 4 + 0) * INPUT_SIZE;
+            const int offset1 = (i * 4 + 1) * INPUT_SIZE;
+            const int offset2 = (i * 4 + 2) * INPUT_SIZE;
+            const int offset3 = (i * 4 + 3) * INPUT_SIZE;
+
+            __m256i sum0 = _mm256_setzero_si256();
+            __m256i sum1 = _mm256_setzero_si256();
+            __m256i sum2 = _mm256_setzero_si256();
+            __m256i sum3 = _mm256_setzero_si256();
+            for (int j = 0; j < NUMBER_OF_INPUT_CHUNKS; j++) {
+                const __m256i in = _mm256_load_si256((const __m256i*)&input[j * REGISTER_WIDTH]);
+
+                Multiply(sum0, in,
+                         _mm256_load_si256((__m256i*)&weights_[offset0 + j * REGISTER_WIDTH]));
+                Multiply(sum1, in,
+                         _mm256_load_si256((__m256i*)&weights_[offset1 + j * REGISTER_WIDTH]));
+                Multiply(sum2, in,
+                         _mm256_load_si256((__m256i*)&weights_[offset2 + j * REGISTER_WIDTH]));
+                Multiply(sum3, in,
+                         _mm256_load_si256((__m256i*)&weights_[offset3 + j * REGISTER_WIDTH]));
+            }
+            const __m128i bias = _mm_load_si128((__m128i*)&biases_[i * 4]);
+            __m128i outval = Add(sum0, sum1, sum2, sum3, bias);
+            _mm_store_si128((__m128i*)&output[i * 4], outval);
+        }
+    }
 
   private:
     void Multiply(__m256i& acc, __m256i a, __m256i b) {
@@ -68,55 +154,32 @@ struct LinearLayer {
         return _mm_add_epi32(_mm_add_epi32(sum128lo, sum128hi), bias);
     }
 
-  public:
-    void Process(const int8_t* input, int32_t* output) {
-        static constexpr int REGISTER_WIDTH = 256 / 8;
-        constexpr int NUMBER_OF_INPUT_CHUNKS = INPUT_SIZE / REGISTER_WIDTH;
-        constexpr int NUMBER_OF_OUTPUT_CHUNKS = OUTPUT_SIZE / 4;
-
-        for (int i = 0; i < NUMBER_OF_OUTPUT_CHUNKS; i++) {
-            const int offset0 = (i * 4 + 0) * INPUT_SIZE;
-            const int offset1 = (i * 4 + 1) * INPUT_SIZE;
-            const int offset2 = (i * 4 + 2) * INPUT_SIZE;
-            const int offset3 = (i * 4 + 3) * INPUT_SIZE;
-
-            __m256i sum0 = _mm256_setzero_si256();
-            __m256i sum1 = _mm256_setzero_si256();
-            __m256i sum2 = _mm256_setzero_si256();
-            __m256i sum3 = _mm256_setzero_si256();
-            for (int j = 0; j < NUMBER_OF_INPUT_CHUNKS; j++) {
-                const __m256i in = _mm256_load_si256((const __m256i*)&input[j * REGISTER_WIDTH]);
-
-                Multiply(
-                    sum0, in, _mm256_load_si256((__m256i*)&weights[offset0 + j * REGISTER_WIDTH]));
-                Multiply(
-                    sum1, in, _mm256_load_si256((__m256i*)&weights[offset1 + j * REGISTER_WIDTH]));
-                Multiply(
-                    sum2, in, _mm256_load_si256((__m256i*)&weights[offset2 + j * REGISTER_WIDTH]));
-                Multiply(
-                    sum3, in, _mm256_load_si256((__m256i*)&weights[offset3 + j * REGISTER_WIDTH]));
-            }
-            const __m128i bias = _mm_load_si128((__m128i*)&biases[i * 4]);
-            __m128i outval = Add(sum0, sum1, sum2, sum3, bias);
-            _mm_store_si128((__m128i*)&output[i * 4], outval);
-        }
-    }
+    alignas(32) std::array<int8_t, INPUT_SIZE * OUTPUT_SIZE> weights_;
+    alignas(32) std::array<int32_t, OUTPUT_SIZE> biases_;
 };
 
-
-template <size_t INPUT_SIZE, size_t OUTPUT_SIZE>
-struct SmallLinearLayer {
-    alignas(32) std::array<std::array<int8_t, OUTPUT_SIZE>, INPUT_SIZE> weights;
-    alignas(32) std::array<int32_t, OUTPUT_SIZE> biases;
-
-    void Process(const int8_t* input, int32_t* buffer) {
-        std::copy(biases.begin(), biases.end(), buffer);
-        for (uint16_t i = 0; i < INPUT_SIZE; i++) {
-            for (uint16_t j = 0; j < OUTPUT_SIZE; j++) {
-                buffer[j] += static_cast<int16_t>(weights[i][j]) * input[i];
-            }
+template <size_t INPUT_SIZE>
+struct OutputLayer {
+  public:
+    void Initialize(ModelReader& reader) {
+        for (size_t i = 0; i < INPUT_SIZE; i++) {
+            weights_[i] = reader.ReadWeight<int8_t>(WEIGHT_SCALE * OUTPUT_SCALE / ACTIVATION_SCALE);
         }
+        bias_ = reader.ReadWeight<int32_t>(WEIGHT_SCALE * OUTPUT_SCALE);
     }
+
+    int32_t Process(const int8_t* input) {
+        int32_t ans = 0;
+        for (uint16_t i = 0; i < INPUT_SIZE; i++) {
+            ans += static_cast<int16_t>(input[i]) * weights_[i];
+        }
+        ans += bias_;
+        return ans;
+    }
+
+  private:
+    alignas(32) std::array<int8_t, INPUT_SIZE> weights_;
+    int32_t bias_;
 };
 
 }  // namespace q_eval
--- src/eval/model.cpp
+++ src/eval/model.cpp
@@ -12,77 +12,26 @@
 namespace q_eval {
 
 static constexpr uint16_t INPUT_LAYER_SIZE = q_core::BOARD_SIZE * q_core::NUMBER_OF_PIECES * 2;
-static constexpr uint16_t FEATURE_LAYER_SIZE = 16;
+static constexpr uint16_t FEATURE_LAYER_SIZE = 32;
 static constexpr uint16_t HIDDEN_LAYER_SIZE = 8;
-
-template <std::integral T>
-T ReadWeight(std::fstream& in, int scale) {
-    float weight;
-    in >> weight;
-    int64_t final_weight = std::round(weight * scale);
-    final_weight = std::min(final_weight, static_cast<int64_t>(std::numeric_limits<T>::max()));
-    final_weight = std::max(final_weight, static_cast<int64_t>(std::numeric_limits<T>::min()));
-    return final_weight;
-}
-
-template <size_t INPUT_SIZE, size_t OUTPUT_SIZE, std::integral T>
-void ReadWeights(std::array<std::array<T, OUTPUT_SIZE>, INPUT_SIZE>& weights, std::fstream& in, float scale) {
-    for (size_t i = 0; i < INPUT_SIZE; i++) {
-        for (size_t j = 0; j < OUTPUT_SIZE; j++) {
-            if (INPUT_SIZE == 32 && i >= 16) {
-                weights[i][j] = 0;
-                continue;
-            }
-            weights[i][j] = ReadWeight<T>(in, scale);
-        }
-    }
-}
-
-template <size_t INPUT_SIZE, size_t OUTPUT_SIZE, std::integral T>
-void ReadWeightsReverse(std::array<T, INPUT_SIZE * OUTPUT_SIZE>& weights, std::fstream& in, float scale) {
-    for (size_t i = 0; i < INPUT_SIZE; i++) {
-        for (size_t j = 0; j < OUTPUT_SIZE; j++) {
-            if (i >= 16) {
-                weights[j * INPUT_SIZE + i] = 0;
-                continue;
-            }
-            weights[j * INPUT_SIZE + i] = ReadWeight<T>(in, scale);
-        }
-    }
-}
-
-template <size_t INPUT_SIZE, std::integral T>
-void ReadBiases(std::array<T, INPUT_SIZE>& biases, std::fstream& in, float scale) {
-    for (size_t i = 0; i < INPUT_SIZE; i++) {
-        biases[i] = ReadWeight<T>(in, scale);
-    }
-}
-
-static constexpr float WEIGHT_SCALE = 64;
-static constexpr float ACTIVATION_SCALE = 127;
-static constexpr float OUTPUT_SCALE = 64;
 
 struct LayerStorage {
     LayerStorage() {
-        std::fstream in("/home/wind-eagle/Quirky/build/avx2_model.qnne");
-        ReadWeights(feature_layer.weights, in, ACTIVATION_SCALE);
-        ReadWeightsReverse<32, 8, int8_t>(hidden_layer.weights, in, WEIGHT_SCALE);
-        ReadWeights(output_layer.weights, in, WEIGHT_SCALE * OUTPUT_SCALE / ACTIVATION_SCALE);
-        ReadBiases(feature_layer.biases, in, ACTIVATION_SCALE);
-        ReadBiases(hidden_layer.biases, in, ACTIVATION_SCALE * WEIGHT_SCALE);
-        ReadBiases(output_layer.biases, in, WEIGHT_SCALE * OUTPUT_SCALE);
+        ModelReader reader;
+        feature_layer.Initialize(reader);
+        hidden_layer.Initialize(reader);
+        output_layer.Initialize(reader);
     }
 
-    FeatureLayer<INPUT_LAYER_SIZE, FEATURE_LAYER_SIZE> feature_layer;
-    LinearLayer<32, HIDDEN_LAYER_SIZE> hidden_layer;
-    SmallLinearLayer<HIDDEN_LAYER_SIZE, 1> output_layer;
+    FeatureLayer<INPUT_LAYER_SIZE, MODEL_INPUT_SIZE> feature_layer;
+    LinearLayer<FEATURE_LAYER_SIZE, HIDDEN_LAYER_SIZE> hidden_layer;
+    OutputLayer<HIDDEN_LAYER_SIZE> output_layer;
 };
 
 static LayerStorage layer_storage{};
 
 void InitializeModelInput(std::array<int16_t, MODEL_INPUT_SIZE>& input) {
-    std::copy(layer_storage.feature_layer.biases.begin(),
-              layer_storage.feature_layer.biases.end(), input.begin());
+    layer_storage.feature_layer.GetResultOnEmptyBoard(input);
 }
 
 void UpdateModelInput(std::array<int16_t, MODEL_INPUT_SIZE>& input, const q_core::cell_t cell,
@@ -98,7 +47,7 @@ void UpdateModelInput(std::array<int16_t, MODEL_INPUT_SIZE>& input, const q_core
 
 score_t ApplyModel(const std::array<int16_t, MODEL_INPUT_SIZE>& input) {
     alignas(32) std::array<int8_t, 32> clamped_input{};
-    for (uint16_t i = 0; i < FEATURE_LAYER_SIZE; i++) {
+    for (uint16_t i = 0; i < MODEL_INPUT_SIZE; i++) {
         clamped_input[i] = std::min(std::max(input[i], static_cast<int16_t>(0)), static_cast<int16_t>(ACTIVATION_SCALE));
     }
     alignas(32) std::array<int32_t, HIDDEN_LAYER_SIZE> buffer;
@@ -107,8 +56,8 @@ score_t ApplyModel(const std::array<int16_t, MODEL_INPUT_SIZE>& input) {
     for (uint16_t i = 0; i < HIDDEN_LAYER_SIZE; i++) {
         hidden_output[i] = std::min(std::max(buffer[i] / static_cast<int32_t>(WEIGHT_SCALE), static_cast<int32_t>(0)), static_cast<int32_t>(ACTIVATION_SCALE));
     }
-    layer_storage.output_layer.Process(hidden_output.data(), buffer.data());
-    return buffer[0] / WEIGHT_SCALE;
+    int32_t ans = layer_storage.output_layer.Process(hidden_output.data());
+    return ans / WEIGHT_SCALE;
 }
 
 }  // namespace q_eval
