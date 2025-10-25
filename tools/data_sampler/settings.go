97988c1282fbc2cde57b1eeb21d0360008a212cb
Anton Kernozhitsky <kernozh_ant2003@mail.ru>
2025-10-25 13:48:17 +0300

    Slightly changed data sampler

--- tools/data_sampler/settings.go
+++ tools/data_sampler/settings.go
@@ -1,14 +1,14 @@
 package main
 
 type Settings struct {
-	GamesInBatch      int    `json:"gamesInBatch"`
-	Batches           int    `json:"batches"`
-	FensInGame        int    `json:"fensInGame"`
-	Cores             int    `json:"cores"`
-	EngineName        string `json:"engineName"`
-	TeacherEngineName string `json:"teacherEngineName"`
-	FilterBinaryName  string `json:"filterBinaryName"`
-	PlayMovetime      int    `json:"playMovetime"`
-	LabelMovetime     int    `json:"labelMovetime"`
-	OpeningBook       string `json:"openingBook"`
+	GamesInBatch      int      `json:"gamesInBatch"`
+	Batches           int      `json:"batches"`
+	FensInGame        int      `json:"fensInGame"`
+	Cores             int      `json:"cores"`
+	EngineNames       []string `json:"engineNames"`
+	TeacherEngineName string   `json:"teacherEngineName"`
+	FilterBinaryName  string   `json:"filterBinaryName"`
+	PlayMovetime      int      `json:"playMovetime"`
+	LabelMovetime     int      `json:"labelMovetime"`
+	OpeningBook       string   `json:"openingBook"`
 }
